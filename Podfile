dd296d6db3e0f8432c77e2a5b4915f4e599509e8
Brian Michel <brian.michel@gmail.com>
2016-12-20 20:58:15 -0500

    Add Sparkle framework

--- Podfile
+++ Podfile
@@ -3,4 +3,5 @@ platform :osx, '10.12'
 use_frameworks!
 target 'Juice' do
   pod 'RxSwift', '~> 3.0.1'
+  pod 'Sparkle', '~> 1.14.0'
 end
--- Pods/Target Support Files/Pods-Juice/Pods-Juice-frameworks.sh
+++ Pods/Target Support Files/Pods-Juice/Pods-Juice-frameworks.sh
@@ -85,7 +85,9 @@ strip_invalid_archs() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/RxSwift/RxSwift.framework"
+  install_framework "${PODS_ROOT}/Sparkle/Sparkle.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/RxSwift/RxSwift.framework"
+  install_framework "${PODS_ROOT}/Sparkle/Sparkle.framework"
 fi
