8598d3130ecef8207d9c04803bf4ace3407951f3
Brian Michel <brian.michel@gmail.com>
2016-12-23 09:52:25 -0500

    Create preflight check and update container

--- fastlane/AppCastUpdater.rb
+++ fastlane/AppCastUpdater.rb
@@ -1,43 +1,61 @@
 require 'nokogiri'
 
-class AppCastUpdater
+# A package object to wrap up the paramters of an update
+class AppCastUpdatePackage
+  attr_accessor :template_path,
+                :archive_path,
+                :title,
+                :description,
+                :download_url,
+                :cast_output_path
+end
+
+# Accepts updater packages and generates appcasts
+class AppCastGenerator
+  SPARKLE_PRIVATE_KEY = 'SPARKLE_PRIVATE_KEY'.freeze
   # Assumes that you have SPARKLE_PRIVATE_KEY and Sparkle bin PATH accessible
-  def prepare_appcast_update(
-    template_path,
-    archive_path,
-    title,
-    description,
-    url,
-    app_cast_output_path)
+  def prepare_appcast_update(package)
+    preflight
 
-    key = ENV["SPARKLE_PRIVATE_KEY"]
+    signature = sign_update(package.archive_path, ENV[SPARKLE_PRIVATE_KEY])
+    length = archive_length(package.archive_path)
 
-    signature = sign_update(archive_path, key)
-    length = archive_length(archive_path)
-
-    write_app_cast(template_path, title, description, url, signature, length, app_cast_output_path)
+    write_app_cast(package, length, signature)
   end
 
-  def write_app_cast(template_path, title, description, url, signature, length, app_cast_output_path)
-    doc = File.open(File.expand_path(template_path)) { |f| Nokogiri::XML(f) }
-    item = doc.xpath("//item").first
-    item.xpath("//title").first.content = title
-    item.xpath("//description").first.content = description
-    item.xpath("//pubDate").first.content = "#{Time.new}"
-    enclosure = item.xpath("//enclosure").first
-    enclosure["url"] = url
-    enclosure["sparkle:version"] = "2.0"
-    enclosure["length"] = length
-    enclosure["sparkle:dsaSignature"] = signature
+  def write_app_cast(package, length, signature)
+    doc = File.open(File.expand_path(package.template_path)) { |f| Nokogiri::XML(f) }
+    item = doc.xpath('//item').first
+    item.xpath('title').first.content = package.title
+    item.xpath('description').first.content = package.description
+    item.xpath('pubDate').first.content = Time.new.to_s
+    enclosure = item.xpath('enclosure').first
+    enclosure['url'] = package.download_url
+    enclosure['sparkle:version'] = '2.0'
+    enclosure['length'] = length
+    enclosure['sparkle:dsaSignature'] = signature
 
-    File.write(File.expand_path(app_cast_output_path), doc.to_xml)
+    File.write(File.expand_path(package.cast_output_path), doc.to_xml)
   end
 
   def sign_update(archive_path, key)
-    return `sign_update #{File.expand_path(archive_path)} #{key}`.chomp
+    signature = `sign_update #{File.expand_path(archive_path)} #{key}`.chomp
+    raise 'Unable to sign update' unless $?.exitstatus == 0
+    return signature
   end
 
   def archive_length(archive_path)
-    return File.size(File.expand_path(archive_path))
+    File.size(File.expand_path(archive_path))
+  end
+
+  def preflight
+    raise 'Unable to find sign_update tool from the Sparkle distribution' unless command? 'sign_update'
+    raise 'SPARKLE_PRIVATE_KEY environment variable is missing!' unless !ENV[SPARKLE_PRIVATE_KEY].nil?
+  end
+
+  private
+
+  def command?(command)
+    system("which #{command} > /dev/null 2>&1")
   end
 end
